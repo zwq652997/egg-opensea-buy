4a5de149f85da1d1f8eeb9383862ce8680b398e7
zwq652997 <zwq652997@sina.com>
2022-10-12 10:35:56 +0800

    6 files modified, 2 added, 2 deleted

--- app/service/home.js
+++ app/service/home.js
@@ -1,4 +1,5 @@
 const Service = require('egg').Service;
+const ethers = require('ethers');
 const { getProvidersInstance } = require('../utils/ProvidersInstance.js');
 const { getSeaportInstance } = require('../utils/SeaportInstance.js');
 
@@ -10,7 +11,7 @@ module.exports = class HomeService extends Service {
   }
   async getOpenseaData() {
     const result = await this.ctx.curl(
-      'https://api.opensea.io/v2/orders/ethereum/seaport/listings?asset_contract_address=0x5b7a317a300699531ca4673473b84b6a7cbbc294&limit=1&token_ids=3697&order_by=created_date&order_direction=desc',
+      'https://api.opensea.io/v2/orders/ethereum/seaport/listings?asset_contract_address=0x92a576207ad9cc1968210bff481f29a790863585&limit=1&token_ids=88&order_by=created_date&order_direction=desc',
       {
         // 自动解析 JSON response
         dataType: 'json',
@@ -28,9 +29,12 @@ module.exports = class HomeService extends Service {
       this.ctx.throw(500, 'order_type not basic');
       return false;
     }
-    return await this.seaportFunction(result.data.orders[0].protocol_data);
+    return await this.seaportFunction(
+      result.data.orders[0].protocol_data,
+      result.data.orders[0].current_price
+    );
   }
-  async seaportFunction(orderInfo) {
+  async seaportFunction(orderInfo, current_price) {
     const { actions, executeAllFulfillActions } =
       await this.seaport.fulfillOrder({
         order: orderInfo,
@@ -41,6 +45,10 @@ module.exports = class HomeService extends Service {
     // await actions[0].transactionMethods.estimateGas();
     let gas = await this.provider.estimateGas(obj);
     let gasLimit = Math.ceil(parseInt(gas) * 1.3);
-    return { ...obj, gasLimit };
+    return {
+      ...obj,
+      gasLimit,
+      price: ethers.utils.formatEther(current_price),
+    };
   }
 };
